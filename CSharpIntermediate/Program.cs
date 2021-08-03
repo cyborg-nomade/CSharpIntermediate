38cd737b21d3d1b9c32690976069e865524288e9
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-03 12:44:18 -0300

    constructors

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -2,31 +2,19 @@
 
 namespace CSharpIntermediate
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I'm {1}", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person
-            {
-                Name = str
-            };
-
-            return person;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = Person.Parse("John");
-            person.Introduce("Uriel");
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
