e10bc976c62096fb95457b80c88384dbacabf3b9
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-03 19:36:13 -0300

    Exercise 1: Design a Stopwatch

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -6,7 +6,18 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine("Press enter to stop");
+            Console.ReadLine();
+            stopwatch.Stop();
+
+            stopwatch.Start();
+
+            Console.WriteLine("Press enter to stop");
+            Console.ReadLine();
+            stopwatch.Stop();
         }
     }
 }
---
+++ CSharpIntermediate/Stopwatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class Stopwatch
+    {
+        private DateTime _currentStartTime;
+        private TimeSpan _currentTimeSpan;
+        public void Start()
+        {
+            this._currentStartTime = DateTime.Now;
+            Console.WriteLine("The stopwatch has been started");
+        }
+
+        public void Stop()
+        {
+            this._currentTimeSpan = DateTime.Now - this._currentStartTime;
+            Console.WriteLine("Stopwatch measure: " + _currentTimeSpan);
+            Reset();
+        }
+
+        public void Reset()
+        {
+            this._currentStartTime = DateTime.MinValue;
+            this._currentTimeSpan = TimeSpan.MinValue;
+        }
+    }
+}
