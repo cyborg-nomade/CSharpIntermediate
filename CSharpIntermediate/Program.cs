276b0521885eccff7307b51d5dd515940982df24
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-03 11:46:58 -0300

    intro to classes

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -2,11 +2,29 @@
 
 namespace CSharpIntermediate
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I'm {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = Person.Parse("John");
+            person.Introduce("Uriel");
         }
     }
 }
