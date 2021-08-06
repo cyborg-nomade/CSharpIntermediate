ad2778216be77fc305df4793a5773612f2a2d921
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-06 17:06:23 -0300

    Exercise 1: Design a database connection
    part 1, developed the classes

---
+++ CSharpIntermediate/DbConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString, TimeSpan timeout) : base(connectionString, timeout)
+        {
+        }
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine("ORACLE OPENED CONNECTION");
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("ORACLE CLOSED CONNECTION");
+        }
+    }
+    public abstract class DbConnection
+    {
+        private string ConnectionString { get; }
+        private TimeSpan Timeout { get; set; }
+
+        public DbConnection(string connectionString, TimeSpan timeout)
+        {
+            if (!String.IsNullOrEmpty(connectionString))
+            {
+                ConnectionString = connectionString;
+            }
+
+            Timeout = timeout;
+        }
+
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+    }
+}
---
+++ CSharpIntermediate/SqlConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class SqlConnection : DbConnection
+    {
+        public override void CloseConnection()
+        {
+            Console.WriteLine("SQL SERVER CLOSED CONNECTIONS");
+        }
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine("SQL SERVER OPENED CONNECTION");
+        }
+
+        public SqlConnection(string connectionString, TimeSpan timeout) : base(connectionString, timeout)
+        {
+        }
+    }
+}
